142d051102879e756afd9db4571a05cb4a8649bd
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-03-14 23:11:15 -0300

    calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+  invisible = true;
+  note1Ready: any;
+  note2Ready: any;
+  sum: any;
+  weight: any;
+  result: any;
+
+  expand(){
+   if(this.invisible === true){
+      this.invisible = false;
+    } else {
+      this.invisible = true;
+    }
+  }
+
+  calc(note1: string, note2: string, weight: string){
+    this.note1Ready = parseInt(note1);
+    this.note2Ready = parseInt(note2);
+    this.weight = parseInt(weight);
+    if(note1 != null && note2 != null){
+      this.result = (this.note1Ready + this.note2Ready) / this.weight
+    }
+  }
 }
+
